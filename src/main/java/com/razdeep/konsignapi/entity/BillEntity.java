246097c734628c2a541bee7d7fd47563ee7c01e4
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2022-09-26 09:25:32 +0530

    fix: unnecessary cascading effect removed from BillEntry
    Signed-off-by: Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>

--- src/main/java/com/razdeep/konsignapi/entity/BillEntity.java
+++ src/main/java/com/razdeep/konsignapi/entity/BillEntity.java
@@ -18,11 +18,11 @@ public class BillEntity {
     @Id
     private String billNo;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne
     @JoinColumn(name = "fk_supplier_id", nullable = false)
     private SupplierEntity supplierEntity;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne
     @JoinColumn(name = "fk_buyer_id", nullable = false)
     private BuyerEntity buyerEntity;
 
@@ -33,25 +33,4 @@ public class BillEntity {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "billEntry")
     private List<LrPmEntity> lrPmEntityList;
-
-//    public BillEntity() {}
-
-//    public BillEntity(Bill bill) {
-//        this.billNo = bill.getBillNo();
-//        this.supplierName = bill.getSupplierName();
-//        this.buyerName = bill.getBuyerName();
-//        this.billDate = bill.getBillDate();
-//        this.transport = bill.getTransport();
-//        this.lrDate = bill.getLrDate();
-//        this.billAmount = bill.getBillAmount();
-//        this.lrPmEntityList = new ArrayList<>();
-//        if (bill.getLrPmList() != null) {
-//            val lrPmList = bill.getLrPmList();
-//            for (int i = 0; i < lrPmList.size(); ++i) {
-//                val lrPmEntity = new LrPmEntity(lrPmList.get(i));
-//                lrPmEntity.setBillEntry(this);
-//                lrPmEntityList.add(lrPmEntity);
-//            }
-//        }
-//    }
 }
