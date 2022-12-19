f253ac79e7a6c217a484bcc48a13f771493aebae
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2022-12-19 17:35:11 +0530

    feat: properly implemented testcontainers
    Signed-off-by: Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>

--- src/test/java/com/razdeep/konsignapi/KonsignApiApplicationTests.java
+++ src/test/java/com/razdeep/konsignapi/KonsignApiApplicationTests.java
@@ -1,13 +1,41 @@
 package com.razdeep.konsignapi;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@ActiveProfiles("ci")
 class KonsignApiApplicationTests {
+
+    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>(DockerImageName.parse("mysql:8.0.24"))
+            .withUsername("root")
+            .withPassword("")
+            .withDatabaseName("konsign");
+    @DynamicPropertySource
+    static void property(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        mySQLContainer.start();
+    }
 
     @Test
     void contextLoads() {
+
+    }
+
+    @AfterAll
+    static void afterAll() {
+        mySQLContainer.stop();
     }
 
 }
