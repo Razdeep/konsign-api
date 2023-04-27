35e2fe7ceabec4a923638daed31b758f3e1510f7
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2023-04-27 22:22:02 +0530

    feat: optimized MasterController
    Signed-off-by: Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>

--- src/main/java/com/razdeep/konsignapi/controller/MasterController.java
+++ src/main/java/com/razdeep/konsignapi/controller/MasterController.java
@@ -1,7 +1,7 @@
 package com.razdeep.konsignapi.controller;
 
-import com.google.gson.Gson;
 import com.razdeep.konsignapi.model.Buyer;
+import com.razdeep.konsignapi.model.ResponseVerdict;
 import com.razdeep.konsignapi.model.Supplier;
 import com.razdeep.konsignapi.model.Transport;
 import com.razdeep.konsignapi.service.BuyerService;
@@ -13,21 +13,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 @RestController
 public class MasterController {
 
-    private final Gson gson;
     private final SupplierService supplierService;
     private final BuyerService buyerService;
     private final TransportService transportService;
 
     @Autowired
-    public MasterController(Gson gson, SupplierService supplierService, BuyerService buyerService, TransportService transportService) {
-        this.gson = gson;
+    public MasterController(SupplierService supplierService, BuyerService buyerService, TransportService transportService) {
         this.supplierService = supplierService;
         this.buyerService = buyerService;
         this.transportService = transportService;
@@ -35,106 +29,106 @@ public class MasterController {
 
     @Timed
     @GetMapping("/suppliers")
-    ResponseEntity<String> getSuppliers() {
-        Map<String, List<Supplier>> message = new HashMap<>();
-        message.put("suppliers", supplierService.getSuppliers());
-        return new ResponseEntity<>(gson.toJson(message), HttpStatus.OK);
+    ResponseEntity<ResponseVerdict> getSuppliers() {
+        ResponseVerdict responseVerdict = new ResponseVerdict();
+        responseVerdict.setData(supplierService.getSuppliers());
+        return new ResponseEntity<>(responseVerdict, HttpStatus.OK);
     }
 
     @Timed
     @PostMapping("/addSupplier")
-    ResponseEntity<String> addSupplier(@RequestBody Supplier supplier) {
-        Map<String, String> messageMap = new HashMap<>();
+    ResponseEntity<ResponseVerdict> addSupplier(@RequestBody Supplier supplier) {
+        ResponseVerdict responseVerdict = new ResponseVerdict();
         if (supplierService.addSupplier(supplier)) {
-            messageMap.put("message", "Successfully added supplier");
-            return new ResponseEntity<>(gson.toJson(messageMap), HttpStatus.OK);
+            responseVerdict.setMessage("Successfully added supplier");
+            return new ResponseEntity<>(responseVerdict, HttpStatus.OK);
         } else {
-            messageMap.put("message", "Failed to add supplier");
-            return new ResponseEntity<>(gson.toJson(messageMap), HttpStatus.BAD_REQUEST);
+            responseVerdict.setMessage("Failed to add supplier");
+            return new ResponseEntity<>(responseVerdict, HttpStatus.BAD_REQUEST);
         }
     }
 
     @Timed
     @DeleteMapping("/supplier/{supplierId}")
-    ResponseEntity<String> deleteSupplier(@PathVariable String supplierId) {
+    ResponseEntity<ResponseVerdict> deleteSupplier(@PathVariable String supplierId) {
         String message;
         if (supplierService.deleteSupplier(supplierId)) {
             message = "Successfully deleted Supplier Id: " + supplierId;
         } else {
             message = supplierId + " is already deleted";
         }
-        Map<String, String> responseMap = new HashMap<>();
-        responseMap.put("message", message);
-        return new ResponseEntity<>(gson.toJson(responseMap), HttpStatus.OK);
+        ResponseVerdict responseVerdict = new ResponseVerdict();
+        responseVerdict.setMessage(message);
+        return new ResponseEntity<>(responseVerdict, HttpStatus.OK);
     }
 
     @Timed
     @GetMapping("/buyers")
-    ResponseEntity<String> getBuyers() {
-        Map<String, List<Buyer>> message = new HashMap<>();
-        message.put("buyers", buyerService.getBuyers());
-        return new ResponseEntity<>(gson.toJson(message), HttpStatus.OK);
+    ResponseEntity<ResponseVerdict> getBuyers() {
+        ResponseVerdict responseVerdict = new ResponseVerdict();
+        responseVerdict.setData(buyerService.getBuyers());
+        return new ResponseEntity<>(responseVerdict, HttpStatus.OK);
     }
 
     @Timed
     @PostMapping("/addBuyer")
-    ResponseEntity<String> addBuyer(@RequestBody Buyer buyer) {
-        Map<String, String> messageMap = new HashMap<>();
+    ResponseEntity<ResponseVerdict> addBuyer(@RequestBody Buyer buyer) {
+        ResponseVerdict responseVerdict = new ResponseVerdict();
         if (buyerService.addBuyer(buyer)) {
-            messageMap.put("message", "Successfully added buyer");
-            return new ResponseEntity<>(gson.toJson(messageMap), HttpStatus.OK);
+            responseVerdict.setMessage("Successfully added buyer");
+            return new ResponseEntity<>(responseVerdict, HttpStatus.OK);
         } else {
-            messageMap.put("message", "Failed to add supplier");
-            return new ResponseEntity<>(gson.toJson(messageMap), HttpStatus.BAD_REQUEST);
+            responseVerdict.setMessage("Failed to add supplier");
+            return new ResponseEntity<>(responseVerdict, HttpStatus.BAD_REQUEST);
         }
     }
 
     @Timed
     @DeleteMapping("/buyer/{buyerId}")
-    ResponseEntity<String> deleteBuyer(@PathVariable String buyerId) {
+    ResponseEntity<ResponseVerdict> deleteBuyer(@PathVariable String buyerId) {
         String message;
         if (buyerService.deleteBuyer(buyerId)) {
             message = "Successfully deleted buyer Id: " + buyerId;
         } else {
             message = buyerId + " is already deleted";
         }
-        Map<String, String> responseMap = new HashMap<>();
-        responseMap.put("message", message);
-        return new ResponseEntity<>(gson.toJson(responseMap), HttpStatus.OK);
+        ResponseVerdict responseVerdict = new ResponseVerdict();
+        responseVerdict.setMessage(message);
+        return new ResponseEntity<>(responseVerdict, HttpStatus.OK);
     }
 
     @Timed
     @PostMapping("/transport")
-    ResponseEntity<String> addTransport(@RequestBody Transport transport) {
-        Map<java.lang.String, java.lang.String> messageMap = new HashMap<>();
+    ResponseEntity<ResponseVerdict> addTransport(@RequestBody Transport transport) {
+        ResponseVerdict responseVerdict = new ResponseVerdict();
         if (transportService.addTransport(transport)) {
-            messageMap.put("message", "Successfully added transport");
-            return new ResponseEntity<>(gson.toJson(messageMap), HttpStatus.OK);
+            responseVerdict.setMessage("Successfully added transport");
+            return new ResponseEntity<>(responseVerdict, HttpStatus.OK);
         } else {
-            messageMap.put("message", "Failed to add transport");
-            return new ResponseEntity<>(gson.toJson(messageMap), HttpStatus.BAD_REQUEST);
+            responseVerdict.setMessage("Failed to add transport");
+            return new ResponseEntity<>(responseVerdict, HttpStatus.BAD_REQUEST);
         }
     }
 
     @Timed
     @GetMapping("/transports")
-    ResponseEntity<String> getTransports() {
-        Map<String, List<Transport>> message = new HashMap<>();
-        message.put("transports", transportService.getTransports());
-        return new ResponseEntity<>(gson.toJson(message), HttpStatus.OK);
+    ResponseEntity<ResponseVerdict> getTransports() {
+        ResponseVerdict responseVerdict = new ResponseVerdict();
+        responseVerdict.setData(transportService.getTransports());
+        return new ResponseEntity<>(responseVerdict, HttpStatus.OK);
     }
 
     @Timed
     @DeleteMapping("/transport/{transportId}")
-    ResponseEntity<String> deleteTransport(@PathVariable String transportId) {
+    ResponseEntity<ResponseVerdict> deleteTransport(@PathVariable String transportId) {
         String message;
         if (transportService.deleteTransport(transportId)) {
             message = "Successfully deleted transport Id: " + transportId;
         } else {
             message = transportId + " is already deleted";
         }
-        Map<String, String> responseMap = new HashMap<>();
-        responseMap.put("message", message);
-        return new ResponseEntity<>(gson.toJson(responseMap), HttpStatus.OK);
+        ResponseVerdict responseVerdict = new ResponseVerdict();
+        responseVerdict.setMessage(message);
+        return new ResponseEntity<>(responseVerdict, HttpStatus.OK);
     }
 }
